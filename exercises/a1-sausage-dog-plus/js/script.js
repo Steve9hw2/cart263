621eeaa542686140fca8ca9543c599da2b337637
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2021-01-23 13:01:42 -0500

    E1: (Almost?) Completed

--- exercises/a1-sausage-dog-plus/js/script.js
+++ exercises/a1-sausage-dog-plus/js/script.js
@@ -1,10 +1,12 @@
 "use strict;"
 
-const NUM_ANIMALS_IMAGES = 10;
-const NUM_ANIMALS = 100;
+const NUM_ANIMALS_IMAGES = 18;
+const NUM_ANIMALS = 180;
 
 const animalImagePrefix = "assets/images/animal"
-const sausageImagePath = "assets/images/sausage-dog.png"
+const sausageImagePath = "assets/images/sausagedog.png"
+const backdropPath = "assets/images/dogpound.png"
+const dogsongPath = "assets/sounds/Dogsong.mp3"
 
 let animalImages = [];
 let animals = [];
@@ -12,22 +14,33 @@ let animals = [];
 let sausageDogImage = undefined;
 let sausageDog = undefined;
 
+let backdrop = undefined;
+let dogsong = undefined;
+let dogsongPlaying = false;
+
+let musicTimer = 0;
+
 function preload() {
     for(let i = 0; i < NUM_ANIMALS_IMAGES; i++) {
       let animalImage = loadImage(`${animalImagePrefix}${i}.png`);
       animalImages.push(animalImage);
     }
     sausageDogImage = loadImage(`${sausageImagePath}`)
+    backdrop = loadImage(`${backdropPath}`)
+    dogsong = loadSound(`${dogsongPath}`)
 };
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    frameRate(30);
+    createCanvas(1215, 810);
     createAnimals();
     createSausageDog();
 };
 
 function draw() {
-    background(255,0,255);
+    image(backdrop,0,0);
+    playDogsong();
+    updateMusicTimer();
     updateAnimals();
     updateSausageDog();
 }
@@ -48,7 +61,7 @@ function createAnimals() {
 function randomizeAnimal() {
   let randomData = {
     x : random(0, width),
-    y : random(0,width),
+    y : random(height/2,height),
     animalImage: random(animalImages)
   }
   return randomData;
@@ -56,7 +69,7 @@ function randomizeAnimal() {
 
 function createSausageDog() {
   let x = random(0, width);
-  let y = random(0, height);
+  let y = random(height/2, height);
   sausageDog = new SausageDog(x, y, sausageDogImage);
 }
 
@@ -68,4 +81,21 @@ function updateAnimals() {
 
 function updateSausageDog() {
   sausageDog.update();
+}
+
+function playDogsong() {
+  if (!dogsongPlaying) {
+    dogsong.play();
+    dogsongPlaying = true;
+  }
+  if (musicTimer >= 1110) {   // frame rate of 30, dogsong lasts 37 seconds
+    musicTimer = 0;
+    dogsongPlaying = false;
+  }
+}
+
+function updateMusicTimer() {
+  if (dogsongPlaying) {
+    musicTimer ++;
+  }
 }
