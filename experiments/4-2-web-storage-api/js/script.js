70b68812717d49fce7f055d3981cb9533cac0b07
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2021-02-04 14:13:03 -0500

    4-2- Web Storage API

---
+++ experiments/4-2-web-storage-api/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let userData = {
+  name: `anonymous`
+};
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  let data = JSON.parse(localStorage.getItem(`web-storage-example-personalization`));
+  if (data !== null) {
+    userData.name = data.name;
+  }
+  else {
+    userData.name = prompt(`What's ya name?`)
+    localStorage.setItem(`web-storage-example-personalization`, JSON.stringify(userData));
+  }
+}
+
+function draw() {
+    background (255);
+
+    push();
+    textSize(64);
+    textAlign(CENTER);
+    text(`Howdy, ${userData.name}!`, width/2, height/2);
+    pop();
+}
