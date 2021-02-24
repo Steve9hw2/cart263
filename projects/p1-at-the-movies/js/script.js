98183483f17ac0c0fab6cd1e7bd9dc941b71f7ef
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2021-02-24 09:07:20 -0500

    P1: Title Added

--- projects/p1-at-the-movies/js/script.js
+++ projects/p1-at-the-movies/js/script.js
@@ -10,7 +10,7 @@ Based on Star Wars???
 **/
 
 let gameState = `title`; // title - selection - firing - running - results
-let numberOfTroopers = 10;
+let numberOfTroopers = 6;
 
 let deathStar;
 let deathStarRoom;
@@ -18,15 +18,30 @@ let deathStarHall;
 
 let starwarsfont;
 
+let trooperimg;
+let trooper2img;
+let trooper3img;
+let trooper4img;
+let trooper5img;
+
 function preload() {
   deathStar = loadImage(`assets/images/deathstar.png`);
   deathStarRoom = loadImage(`assets/images/deathstarroom.png`)
   deathStarHall = loadImage(`assets/images/deathstarwall.png`)
   starwarsfont = loadFont(`assets/fonts/Starjedi.ttf`);
+  //troopers
+  trooperimg = loadImage(`assets/images/trooper.png`);
+  trooper2img = loadImage(`assets/images/trooper2.png`);
+  trooper3img = loadImage(`assets/images/trooper3.png`);
+  trooper4img = loadImage(`assets/images/trooper4.png`);
+  trooper5img = loadImage(`assets/images/trooper5.png`);
 }
 
 function setup() {
   createCanvas(1800,900);
+  for(let i; i < numberOfTroopers; i++) {
+    
+  }
 }
 
 function draw() {
@@ -35,8 +50,10 @@ function draw() {
     case `title`:
     image(deathStar,0,0);
     drawTitle();
+    drawPlayButton();
     break;
     case `selection`:
+    image(deathStarHall,0,0);
     break;
     case `firing`:
     break;
@@ -55,4 +72,36 @@ function drawTitle() {
   text(`Storm Trooper`, 200, 300)
   text(`Training Simulator`, 200, 340)
   pop();
+}
+
+function drawPlayButton() {
+  print(mouseX,mouseY);
+  push();
+  if (mouseX >= 190 && mouseX <= 370 && mouseY >= 550 && mouseY <= 600) {
+    fill(150);
+  } else {
+    fill(255);
+  }
+  textFont(starwarsfont);
+  textSize(60);
+  text(`Play`, 200, 600);
+  pop();
+}
+
+function mousePressed() {
+  switch(gameState) {
+    case `title`:
+    if (mouseX >= 190 && mouseX <= 370 && mouseY >= 550 && mouseY <= 600) {
+      gameState = `selection`;
+    }
+    break;
+    case `selection`:
+    break;
+    case `firing`:
+    break;
+    case `running`:
+    break;
+    case `results`:
+    break;
+  }
 }
