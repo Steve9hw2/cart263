9f65c8fbcf202d7b572cd301b258cb1a0ca7787b
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2021-03-16 13:32:42 -0400

    E6: 🦀 and comments

--- exercises/e6-raving-redactionist-plus/js/script.js
+++ exercises/e6-raving-redactionist-plus/js/script.js
@@ -4,11 +4,12 @@ $(`.top-secret`).on(`click`, redact);
 
 setInterval(revelation, 500);
 
-$(`#failure`).hide();
-let speakText = ` `;
-let speaking = false;
+$(`#failure`).hide(); // hiding what appears in the fail state
+$(`#crabbed`).hide();
+let speakText = ` `; // this is the value which will be changed to allow responsiveVoice to speak
+let speaking = false; // this bool ensures the "hacker" cannot read two things at once
 
-let music = new Audio(`assets/sounds/Noisestorm-CrabRave.mp3`)
+let music = new Audio(`assets/sounds/Noisestorm-CrabRave.mp3`) // totally official government
 
 function redact(event) {
   $(this).removeClass(`revealed`);
@@ -41,17 +42,23 @@ function attemptReveal() {
   }
 }
 
-function checkFailure() {
+function checkFailure() { // check that all text has been revealed, once the voice is done speaking
   if ($(`.redacted`).length === 0) {
     music.play();
     $(`#secret-document`).hide();
     $(`#score`).hide();
-    setTimeout(showFailure, 32000);
+    setTimeout(showFailure, 31000);
   }
 }
 
-function showFailure() {
+function showFailure() { // show the funny 🦀
   $(`#failure`).show();
+  setTimeout(revealAltered, 110000);
+}
+
+function revealAltered() { // show the *final* end screen
+  $(`#failure`).hide();
+  $(`#crabbed`).show();
 }
 // i used this as a reference for playing music without a preload-
 // https://www.codegrepper.com/code-examples/javascript/jquery+play+audio
