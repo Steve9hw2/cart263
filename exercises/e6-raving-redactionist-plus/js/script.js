df04952d3b8bbea66c72eaa50cf411ef1d149297
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2021-03-16 13:17:18 -0400

    E6: Failstate, Redacting lines, Audio & Timeout

--- exercises/e6-raving-redactionist-plus/js/script.js
+++ exercises/e6-raving-redactionist-plus/js/script.js
@@ -4,16 +4,20 @@ $(`.top-secret`).on(`click`, redact);
 
 setInterval(revelation, 500);
 
+$(`#failure`).hide();
 let speakText = ` `;
 let speaking = false;
+
+let music = new Audio(`assets/sounds/Noisestorm-CrabRave.mp3`)
 
 function redact(event) {
   $(this).removeClass(`revealed`);
   $(this).addClass(`redacted`);
   let previousScore = $(`#score-number`).text();
-  let newScore = previousScore++;
+  let newScore = previousScore;
+  newScore++;
   $(`#score-number`).text(newScore);
-  if ($(this).text() === speakText) {
+  if ($(this).text() === speakText) { // this if cancels the speaking of the last spoken line of text if it's interrupted by being redacted.
     responsiveVoice.cancel();
     speaking = false;
   }
@@ -32,6 +36,22 @@ function attemptReveal() {
     speakText = $(this).text();
     responsiveVoice.speak(speakText, "Bosnian Male", {onend: function(){
       speaking = false;
+      checkFailure();
     }});
   }
 }
+
+function checkFailure() {
+  if ($(`.redacted`).length === 0) {
+    music.play();
+    $(`#secret-document`).hide();
+    $(`#score`).hide();
+    setTimeout(showFailure, 32000);
+  }
+}
+
+function showFailure() {
+  $(`#failure`).show();
+}
+// i used this as a reference for playing music without a preload-
+// https://www.codegrepper.com/code-examples/javascript/jquery+play+audio
