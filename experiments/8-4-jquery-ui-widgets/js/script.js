7c4fee175c3aad0e2bfe194b6f8c58a412f036ee
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2021-03-22 19:08:26 -0400

    8-4: jQuery UI Widgets

---
+++ experiments/8-4-jquery-ui-widgets/js/script.js
@@ -0,0 +1,50 @@
+$(`#escape-tunnel`).hide();
+
+$(`#introduction-dialog`).dialog({
+  resizable: false,
+  modal: true,
+  buttons: {
+    "Imagination": function() {
+      $(`#prisoner`).draggable(`option`, `containment`, `none`);
+      $(this).dialog(`close`);
+    },
+    "Escape tunnel": function() {
+      $(`#escape-tunnel`).show({
+        effect: `blind`
+      });
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+
+$(`#prisoner`).effect({
+  effect: `shake`,
+  duration: 2000,
+  times: 15,
+  distance: 10,
+  complete: makeDraggable
+});
+
+
+$(`#escape-tunnel`).droppable({
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    $(this).hide({
+      effect: `blind`,
+      duration: 500
+    });
+  }
+})
+
+function makeDraggable() {
+  $(`#prisoner`).draggable({
+    containment: "#prison",
+    start: function(event, ui) {
+      $(this).addClass(`prisoner-dragging`, 750);
+    },
+    stop: function(event, ui) {
+      $(this).removeClass(`prisoner-dragging`, 750);
+    }
+  });
+}
