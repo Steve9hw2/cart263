2b61f90277e3c1f7ce1c0f010e54d27a423b9e78
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2021-03-30 09:16:00 -0400

    P2: Added Textbox & JSON*
    *Nothing seems to be working though

--- projects/p2-vnf/js/prototype.js
+++ projects/p2-vnf/js/prototype.js
@@ -1,1 +1,19 @@
+"use strict";
+let textJSON;
+$(document).ready(function() {
+  $.getJSON("dialogue/prototype.json", function(data){
+    console.log(data.dialogue)
+  }).fail(function(){
+    console.log("An error has occurred.");
+  });
+});
+
+
+console.log(`${textJSON}`)
+let currentIndex = textJSON.dialogue.one
+$(`#text`).text(currentIndex);
+
 console.log(`script prints`)
+$(`#cr_enthused`).hide();
+$(`#cr_pain`).hide();
+$(`#cr_neutral`).hide();
