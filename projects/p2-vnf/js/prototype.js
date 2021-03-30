20d57c48243f499bb0357c5c40c88ad0e825742f
Steve9hw2 <70410761+Steve9hw2@users.noreply.github.com>
2021-03-30 14:46:41 -0400

    P2: Portraits Fixed!

--- projects/p2-vnf/js/prototype.js
+++ projects/p2-vnf/js/prototype.js
@@ -1,8 +1,10 @@
 "use strict";
 let textJSON;
+let currentIndex;
 $(document).ready(function() {
   $.getJSON("dialogue/prototype.json", function(data){
     console.log(data.dialogue)
+    currentIndex = textJSON.dialogue[0].one[0].text
   }).fail(function(){
     console.log("An error has occurred.");
   });
@@ -10,10 +12,14 @@ $(document).ready(function() {
 
 
 console.log(`${textJSON}`)
-let currentIndex = textJSON.dialogue.one
+
 $(`#text`).text(currentIndex);
 
 console.log(`script prints`)
 $(`#cr_enthused`).hide();
 $(`#cr_pain`).hide();
 $(`#cr_neutral`).hide();
+$(`#cr_pleased`).hide();
+$(`#cr_shock`).hide();
+$(`#cr_tears`).hide();
+$(`#cr_unsure`).hide();
